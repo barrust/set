52b8015d4b72897b3a197543c620417e1639019c
Tyler Barrus <barrust@gmail.com>
2016-05-08 20:46:31 -0400

    updated usage section; cleanded up some of the test

--- README.md
+++ README.md
@@ -10,6 +10,32 @@ MIT 2016
 
 ##Usage:
 ```
+#include "set.h"
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    SimpleSet set;
+    set_init(&set);
+    set_add(&set, "orange");
+    set_add(&set, "blue");
+    set_add(&set, "red");
+    set_add(&set, "green");
+    set_add(&set, "yellow");
+
+    if (set_contains(&set, "yellow") == SET_TRUE) {
+        printf("Set contains 'yellow'!\n");
+    } else {
+        printf("Set does not contains 'yellow'!\n");
+    }
+
+    if (set_contains(&set, "purple") == SET_TRUE) {
+        printf("Set contains 'purple'!\n");
+    } else {
+        printf("Set does not contains 'purple'!\n");
+    }
+
+    set_destroy(&set);
+}
 ```
 
 ##Required Compile Flags:
--- tests/set_test.c
+++ tests/set_test.c
@@ -5,6 +5,20 @@
 
 #define KEY_LEN 25
 
+void check_string(SimpleSet *set, char *key) {
+    if (set_contains(set, key) == SET_TRUE) {
+        printf("Set contains [%s]!\n", key);
+    } else {
+        printf("Set does not contains [%s]!\n", key);
+    }
+}
+
+void missing_key_check(SimpleSet *set, char *key) {
+    if (set_contains(set, key) != SET_TRUE) {
+        printf("Missing key! [%s]\n", key);
+    }
+}
+
 int main(int argc, char **argv) {
     SimpleSet set;
     set_init(&set);
@@ -18,21 +32,19 @@ int main(int argc, char **argv) {
             printf("Error inserting: %s\tres: %d\n", key, res);
         }
     }
-    printf("Set contains [%s]: %d\n", "foo", set_contains(&set, "foo"));
-    printf("Set contains [%s]: %d\n", "test", set_contains(&set, "test"));
-    printf("Set contains [%s]: %d\n", "bar", set_contains(&set, "bar"));
+    check_string(&set, "foo");
+    check_string(&set, "test");
+    check_string(&set, "bar");
 
     printf("Remove [test]....\n");
     set_remove(&set, "test");
-    printf("Set contains [%s]: %d\n", "test", set_contains(&set, "test"));
+    check_string(&set, "test");
 
     printf("Test inclusion of other elements...\n");
     for (i = 0; i < 50000000; i++) {
         char key[KEY_LEN] = {0};
 		sprintf(key, "%d", i);
-        if (set_contains(&set, key) != SET_TRUE) {
-            printf("Missing key! [%s]\n", key);
-        }
+        missing_key_check(&set, key);
     }
     printf("Completed testing other elements in set\n");
 
